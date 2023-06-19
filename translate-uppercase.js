a8bb9b490e477a8488c26d77e03a635f46552eef
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-19 18:53:13 +0300

    jsinfo exercise

---
+++ translate-uppercase.js
@@ -0,0 +1,14 @@
+// Write the function camelize(str) that changes dash-separated words like “my-short-string” into camel-cased “myShortString”.
+
+// That is: removes all dashes, each word after dash becomes uppercased.
+
+function camelize(str) {
+  return str.split('-').map((word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1)).join('')
+}
+
+console.log(camelize("background-color"))
+// 'backgroundColor'
+console.log(camelize("list-style-image"))
+// 'listStyleImage'
+console.log(camelize("-webkit-transition"))
+// 'WebkitTransition'
