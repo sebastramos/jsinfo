72c444fd730f19ab44d5760ac5265ccdc1114122
Sebastian Ramos <sebramos.dev@gmail.com>
2023-06-20 17:53:22 +0300

    jsinfo exercise

---
+++ map-to-objects.js
@@ -0,0 +1,26 @@
+// You have an array of user objects, each one has name, surname and id.
+
+// Write the code to create another array from it, of objects with id and fullName, where fullName is generated from name and surname.
+
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+let users = [ john, pete, mary ];
+
+  let usersMapped = users.map(element => ({
+  fullName: `${element.name} ${element.surname}`,
+  id: element.id
+  }))
+
+/*
+usersMapped = [
+  { fullName: "John Smith", id: 1 },
+  { fullName: "Pete Hunt", id: 2 },
+  { fullName: "Mary Key", id: 3 }
+]
+*/
+
+console.log( usersMapped[0].id ) // 1
+console.log( usersMapped[0].fullName ) // John Smith
+// So, actually you need to map one array of objects to another. Try using => here. There’s a small catch.
