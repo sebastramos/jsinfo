7381e54a930424c9336c5198b9922b1d23d630e3
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-21 20:08:51 +0300

    jsinfo exercise

--- sum-object-properties.js
+++ sum-object-properties.js
@@ -9,10 +9,15 @@ let salaries = {
 // Write the code to sum all salaries and store in the variable sum. Should be 390 in the example above.
 
 // If salaries is empty, then the result must be 0.
+
+// Solution 1
 let sum = 0
 for(let salary in salaries) {
 	sum += +salaries[salary]
 }
 
 console.log(sum)
+
+// Solution 2
+console.log(Object.values(salaries).reduce((a,b)=> a + b))
 
