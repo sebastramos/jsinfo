d378c5627c8d1e8d87c348b7ce91d30230a28efa
Sebastian Ramos <sebramos.dev@gmail.com>
2023-07-24 18:18:57 +0300

    jsinfo exercise

---
+++ Objects/create-new-accumulator.js
@@ -0,0 +1,27 @@
+// Create a constructor function Accumulator(startingValue).
+
+// Object that it creates should:
+
+// Store the “current value” in the property value.
+
+// The starting value is set to the argument of the constructor startingValue.
+
+// The read() method should use prompt to read a new number and add it to value.
+
+// In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+
+function Accumulator(startingValue) {
+	this.value = startingValue
+  
+	this.read = function() {
+  	this.value += +prompt('How much to add ?', 0)
+  }
+  
+}
+
+let accumulator = new Accumulator(1); // initial value 1
+
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+
+console.log(accumulator.value); // shows the sum of these values
